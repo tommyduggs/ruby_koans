d3f2e143ea20c3f3c580a0a0655aa925ba52ed29
Tom Duggan <tom13dm@gmail.com>
2024-04-07 17:39:02 -0400

    blocks, exceptions, iterations

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,11 +14,26 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  return :equilateral if (a == b and b == c)
-  return :isosceles if (a == b or a == c or b == c)
+  validate_triangle(a, b, c)
+
+  return :equilateral if (a == b && b == c)
+  return :isosceles if (a == b || a == c || b == c)
   return :scalene
 end
 
-# Error class used in part 2.  No need to change this code.
+def validate_triangle(a, b, c)
+  side_lengths = [a, b, c].sort
+  side_lengths.each do |side_length|
+    raise TriangleError, "The values passed in must be numeric" unless side_length.is_a?(Numeric)
+    raise TriangleError, "The length of a side may not be zero" if side_length == 0
+    raise TriangleError, "The length of a side may not be negative" if side_length <= 0
+  end
+
+  if (side_lengths[0] + side_lengths[1]) <= side_lengths[2]
+    raise TriangleError, "The sum of the two shorter sides must be greater than the longest side"
+  end
+end
+
+# Error class used in part 0.  No need to change this code.
 class TriangleError < StandardError
 end
